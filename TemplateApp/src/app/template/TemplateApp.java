05bc81e83c3e76336d93fb3ceddae5d4fe20b4ba
Pawel Kwiecinski <pawel.kwiecinski@whallalabs.com>
2013-08-08 23:14:19 +0200

    Added UIL library.

--- TemplateApp/src/app/template/TemplateApp.java
+++ TemplateApp/src/app/template/TemplateApp.java
@@ -1,6 +1,17 @@
 package app.template;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
 import org.holoeverywhere.app.Application;
 
 public class TemplateApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ImageLoaderConfiguration imageLoaderConfiguration = ImageLoaderConfiguration.createDefault(getApplicationContext());
+        ImageLoader.getInstance().init(imageLoaderConfiguration);
+    }
 }
